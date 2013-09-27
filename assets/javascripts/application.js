4ff9b5be22b736d29e800568e5aa0c4d98d949d7
Thomas Mango <tsmango@gmail.com>
2013-09-26 21:16:51 -0400

    Sponsors are now dynamically loaded.

---
+++ assets/javascripts/application.js
@@ -0,0 +1,34 @@
+(function() {
+  var RailsRumble;
+
+  RailsRumble = {
+    initialize: function() {
+      RailsRumble.fetchSponsors();
+    },
+    fetchSponsors: function() {
+      $.ajax({
+        url: 'http://railsrumble.com/sponsors.json',
+        data: {},
+        dataType: 'jsonp',
+        jsonp: 'callback',
+        jsonpCallback: 'RailsRumble.renderSponsors',
+        success: function(){
+          alert("success");
+        }
+      });
+    },
+    renderSponsors: function(data) {
+      try {
+        $('#sponsors h1').text(data['competition']['year'] + ' Rumble Sponsors');
+        _.each(data['competition']['sponsors'], function(sponsor, position) {
+          image = $('<img/>').attr('src', sponsor['image_for_sidebar']);
+          link  = $('<a></a>').attr('href', sponsor['url']).attr('target', '_blank');
+          $('#sponsors').append($('<div></div>').append(link.append(image)));
+        });
+      }catch(a){}
+    }
+  };
+
+  window.RailsRumble = RailsRumble;
+
+}).call(this);
