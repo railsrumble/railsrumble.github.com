07c5613852d0a76ad320211f3133df45beb25ec3
Thomas Mango <tsmango@gmail.com>
2013-10-03 10:16:21 -0400

    The sponsorship interest request now hides when the sponsors are current.

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -20,6 +20,11 @@
     renderSponsors: function(data) {
       try {
         $('#sponsors h1').text(data['competition']['year'] + ' Rumble Sponsors');
+
+        if(data['competition']['current']) {
+          $('#sponsorship-interest').parents('article').hide();
+        }
+
         _.each(_.sortBy(data['competition']['sponsors'], function(sponsor){ return sponsor['position']; }), function(sponsor, position) {
           image = $('<img/>').attr('src', sponsor['image_for_sidebar']);
           link  = $('<a></a>').attr('href', sponsor['url']).attr('target', '_blank');
